38716d792c1ec1d8ad07626fdec6dc20d14ec9eb
Breno Duarte <brenoduarte239@gmail.com>
2023-04-10 11:39:35 -0300

    Inicio

---
+++ CursoJavaScript/revisao-logica/variaveis.js
@@ -0,0 +1,15 @@
+// var, let, const
+
+let teste = "Minha string"
+teste = 10;
+
+const teste2 = 10;
+
+let teste3
+console.log(teste2);
+
+teste3 = 20;
+console.log(teste3);
+
+const teste4 = "eu sou imutavel";
+console.log(teste4);
