05c62adda91af332afb9bf1da3cf443984cdb63e
Breno Duarte <brenoduarte239@gmail.com>
2023-04-18 20:21:46 -0300

    aa

--- CursoJavaScript/DOM/ex/ex002/js/saudacao.js
+++ CursoJavaScript/DOM/ex/ex002/js/saudacao.js
@@ -1,4 +1,12 @@
-(function () {
+const txtEmail = document.getElementById("txtEmail")
+const msgFeedback = document.getElementById("newsletterFeedback")
 
 
-})()
+
+
+
+function cadastrarEmail() {
+    let email = txtEmail.value
+    msgFeedback.innerHTML = `O email ${email} foi cadastrado com sucesso`
+    
+} 
